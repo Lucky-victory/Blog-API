72bc88c56789ed0f4e098f0535a2e23c2b886cb1
Lucky Victory <Blizwonder25@gmail.com>
2022-03-13 17:06:35 +0000

    refactoring articles.js

--- controllers/articles.js
+++ controllers/articles.js
@@ -11,15 +11,16 @@ const getArticles=async(req,res)=>{
 
       const {limit=20,page=1}=req.query;
       const offset=(parseInt(limit) * parseInt(page)) - limit || 0;
-      const articles=await Articles.find({getAttributes:["title","body","tags","publishedAt","modifiedAt","authorId","heroImage","id","category"],limit,offset});
+      const articles=await Articles.find({getAttributes:["title","body","tags","publishedAt","modifiedAt","authorId","heroImage","id","category","slug"],limit,offset});
       if(articles.length){
+         // get authorIds from articles
          let articleAuthorsId=articles.map(({authorId})=>authorId);   
-         
+         // use those authorId to find the article authors from Authors table
          const authors=await Authors.findById({getAttributes:["fullname","twitter","linkedIn","id"],id:articleAuthorsId});
+         // now join the articles with their respective articles
          const articlesWithAuthor=articles.map((article)=>{
             return {...article,'author':{...authors.reduce((accum,author)=>{ article.authorId==author.id ? accum=author:accum; return accum;},{})}}
                });
-
 
                res.status(200).json({"message":"Articles retrieved",status:200,data:articlesWithAuthor,result_count:articlesWithAuthor.length});
                return
@@ -30,15 +31,22 @@ const getArticles=async(req,res)=>{
             res.status(400).json({"error":err})
          }
 }
+
+// Gte all article tags
 const getTags=async(req,res)=>{
 const {limit=20,page=1}=req.query;
    const offset=(parseInt(limit) * parseInt(page)) - limit || 0;
+   // select article tags from Articles table, this returns an array of objects with tags props
    let allTags=await Articles.find({getAttributes:["tags"],limit,offset});
+
+// filter out null tags, and flatten the object into an array of strings
    allTags=[...allTags.reduce((accum,t)=>{t.tags !=null ? accum.push(t.tags):accum
    return accum;
    },[])]
 res.status(200).json({"message":"Tags retrieved",status:200,"tags":allTags})
 }
+
+// Get all article categories
 const getCategories=async(req,res)=>{
    const {limit=20,page=1}=req.query;
       const offset=(parseInt(limit) * parseInt(page)) - limit || 0;
@@ -47,14 +55,15 @@ const getCategories=async(req,res)=>{
    res.status(200).json({"message":"Categories retrieved",status:200,categories})
    }
 
-   
+   // Add new article
 const createNewArticle=async(req,res)=>{
    if(req.body && !Object.keys(req.body).length){
       res.status(400).json({"message":"Please include article to add",status:400})
       return;
    }
-
-   const newArticle=req.body || {};
+const currentDate=new Date().toISOString();
+const {publishedAt=currentDate, }=req.body;
+   const newArticle= {};
    const {title}=newArticle;
 const slug=slugify(title,{lower:true,strict:true})+"-"+shortId();
    const {heroImage="https://images.unsplash.com/photo-1542435503-956c469947f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"}=newArticle;
--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express=require('express');
 const app=express();
 const PORT=process.env.PORT ||4901;
+
+// middleware to accept json body requests.
 app.use(express.json({extended:false}));
+// middleware to accept a form 
 app.use(express.urlencoded({extended:false}));
 
-
+// articles route
 const articlesRouter=require('./routes/articles');
+// single article route
 const articleRouter=require('./routes/article');
+// users route
 const usersRouter=require("./routes/authors");
 
 app.use('/articles',articlesRouter);
