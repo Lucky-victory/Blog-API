4b72d27b5077254e14432d7862232d75d850783e
Lucky Victory <65457498+Lucky-victory@users.noreply.github.com>
2022-03-14 22:49:12 +0100

    Update articles.js

--- controllers/articles.js
+++ controllers/articles.js
@@ -14,18 +14,20 @@ const getArticles=asyncHandler(async(req,res)=>{
       const articles=await Articles.find({getAttributes:["title","body","tags","publishedAt","modifiedAt","authorId","heroImage","id","category","slug","views"],limit,offset});
       if(articles.length){
          // get authorIds from articles
-         let articleAuthorsId=articles.map(({authorId})=>authorId);   
+         let articleAuthorsId=articles.map(({authorId})=>authorId);  
+ 
          // use those authorId to find the article authors from Authors table
          const authors=await Authors.findById({getAttributes:["fullname","twitter","linkedIn","id"],id:articleAuthorsId});
-         // now join the articles with their respective articles
+
+         // now join the authors with their respective articles
          const articlesWithAuthor=articles.map((article)=>{
             return {...article,'author':{...authors.reduce((accum,author)=>{ article.authorId==author.id ? accum=author:accum; return accum;},{})}}
                });
 
-               res.status(200).json({"message":"Articles retrieved",status:200,data:articlesWithAuthor,result_count:articlesWithAuthor.length});
+               res.status(200).json({"message":"Articles retrieved",status:200,"articles":articlesWithAuthor,result_count:articlesWithAuthor.length,page});
                return
             }
-            res.status(200).json({"message":"No Articles"})
+            res.status(200).json({"message":"No Articles","articles":null})
          }
          catch(err){
             const status=err.status ||400;
@@ -33,7 +35,7 @@ const getArticles=asyncHandler(async(req,res)=>{
           }
 });
 
-// Gte all article tags
+// Get all article tags
 const getTags= asyncHandler(async(req,res)=>{
    try{
 
@@ -80,12 +82,10 @@ const createNewArticle=asyncHandler( async(req,res)=>{
          return;
       }
       const currentDate=new Date().toISOString();
-      const {publishedAt=currentDate,heroImage='https://images.pexels.com/photos/4458/cup-mug-desk-office.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',title,content,tags, }=req.body;
+      const {publishedAt=currentDate,category,heroImage='https://images.pexels.com/photos/4458/cup-mug-desk-office.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',title,content,tags }=req.body;
       const slug=slugify(title,{lower:true,strict:true})+"-"+shortId();
       const newArticle= {publishedAt,title,content,tags,heroImage,slug,category,authorId};
       newArticle['modifiedAt']=publishedAt;
-      newArticle["heroImage"]=heroImage;
-      newArticle["slug"]=slug;
       newArticle["views"]=0;
       
       await Articles.create(newArticle);
