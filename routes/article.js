684b7f6cba23059c29a0aaf6056b75ff9dabaff5
lucky-victory <blizwonder25@gmail.com>
2022-04-02 18:33:32 +0100

    added cors middleware

--- routes/article.js
+++ routes/article.js
@@ -1,7 +1,7 @@
 const router=require('express').Router();
-const {getArticleBySlug,deleteArticle,editArticle}=require('../controllers/article')
-
-router.get('/:slug',getArticleBySlug);
+const {getArticleBySlug,deleteArticle,editArticle}=require('../controllers/article');
+const cors=require("cors");
+router.get('/:slug',cors(),getArticleBySlug);
 router.put('/edit/:articleId',editArticle);
 router.delete('/delete/:articleId',deleteArticle);
 
