a790f0ef5de2c3ae605a5504eb2fccd328561757
Lucky Victory <luckyvictory54@gmail.com>
2022-04-02 18:08:36 +0100

    Update authors.js

--- models/authors.js
+++ models/authors.js
@@ -1,6 +1,6 @@
 const {Schema,Model}=require("harpee");
 
-const ArticlesSchema=new Schema({name:"B2Schema",fields:{
+const ArticlesSchema=new Schema({name:"ArticlesSchema",fields:{
    fullname:String,
    username:String,
    email:String,
