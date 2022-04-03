d5063ba2778ea6094c1e52a472c025690d78cd63
lucky-victory <blizwonder25@gmail.com>
2022-04-03 14:38:43 +0100

    [fix] profileImage

--- controllers/article.js
+++ controllers/article.js
@@ -21,11 +21,11 @@ const getArticleBySlug= asyncHandler (async (req,res)=>{
 const {title,content,publishedAt,modifiedAt,slug,heroImage,id,authorId,category,views}=article;
 let {tags}=article;
 tags= tags? [tags.split(",").join('","') ]:null
-const {fullname,twitter,linkedIn,bio,profileImage}=await Authors.findOne({"id":authorId});
+const {fullname,twitter,linkedIn,bio,profileImage,username}=await Authors.findOne({"id":authorId});
 
 const newViewsCount=parseInt(views)+1 || 1;
  await Articles.update([{id,'views':newViewsCount}]);
-res.status(200).json({title,content,slug,views,publishedAt,modifiedAt,tags,heroImage,id,category,author:{fullname,twitter,linkedIn}});
+res.status(200).json({title,content,slug,views,publishedAt,modifiedAt,tags,heroImage,id,category,author:{fullname,twitter,linkedIn,profileImage,bio,username}});
 
 }
 catch(err){
