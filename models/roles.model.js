017a0b31a275d871a15c30266bd0d73c2ed383b3
Lucky-victory <blizwonder25@gmail.com>
2022-05-29 16:02:51 +0100

    refactored models & controllers

--- models/roles.model.js
+++
@@ -1,9 +0,0 @@
-const {Schema,Model}=require("harpee");
-
-const BlogSchema=new Schema({name:"BlogSchema",fields:{
-   createdAt:Date,
-     role:String
-},silent:true,primaryKey:'id'});
-
-const Roles=new Model("Roles",BlogSchema);
-module.exports=Roles;
